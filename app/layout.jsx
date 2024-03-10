4bf6c74d4599193842628f2736bdd3dbf277d9d3
Andrea <zorzi.andrea@outlook.com>
2024-03-11 00:31:29 +0100

    feat: update README.md & added Vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,27 @@
-import "@styles/globals.css";
-
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
+import '@styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
 };
 
 const RootLayout = ({ children }) => (
-  <html lang='en'>
+  <html lang="en">
     <body>
       <Provider>
-        <div className='main'>
-          <div className='gradient' />
+        <div className="main">
+          <div className="gradient" />
         </div>
 
-        <main className='app'>
+        <main className="app">
           <Nav />
           {children}
         </main>
       </Provider>
+      <SpeedInsights />
     </body>
   </html>
 );
